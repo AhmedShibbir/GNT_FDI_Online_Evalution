86d28dd811cdba58e9cc522fd9a0328b3474145c
Shibbir Ahmed <shibbirahmedops@gmail.com>
2021-10-18 16:04:02 +0600

    customer list added

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function modalOperation(){
                 <a id = "addCustomer" class="text-white text-decoration-none my-5 px-5 py-2 bg-success bg-opacity-50 fw-bold border" src="/index.html">Add Customer</a>
                 <a id ="exit" class="text-white text-decoration-none my-5 px-5 py-2 bg-success bg-opacity-50 fw-bold border" src="/index.html">Exit</a>
             </div>
-            <div class=" bg-success w-75 d-flex py-5 flex-column align-items-center">
+            <div id = "infoPart" class=" bg-success w-75 d-flex py-5 flex-column align-items-center">
                 <h1>Welcome, There!</h1>
                 <p>Thanks for being with us!</p>
             </div>
@@ -71,6 +71,73 @@ function dashboardOperation(){
         document.getElementById("body").removeChild(dashboard);
         count=0;
     });
+    const customerList = document.getElementById("customerList");
+    customerList.addEventListener("click",()=>{
+        const customerTable = document.createElement("div");
+        customerTable.innerHTML = `
+        <h1 class="mb-3">Customer List: </h1>
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">UserName</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Gender</th>
+                    <th scope="col">City</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Arif Azad</td>
+                    <td>arif001</td>
+                    <td>arif@gmail.com</td>
+                    <td>Male</td>
+                    <td>Rajshahi</td>
+                </tr>
+                <tr>
+                    <td>Sazzad Said</td>
+                    <td>said55s</td>
+                    <td>sazzad055@gmail.com</td>
+                    <td>Male</td>
+                    <td>Rangpur</td>
+                </tr>
+                <tr>
+                    <td>Rokeya Ahona</td>
+                    <td>ahona123r</td>
+                    <td>rokeya12.ohona@gmail.com</td>
+                    <td>Female</td>
+                    <td>Noakhali</td>
+                </tr>
+                <tr>
+                    <td>Heron Maula</td>
+                    <td>hmaula12</td>
+                    <td>heronmaula.bd@gmail.com</td>
+                    <td>Male</td>
+                    <td>Narayanganj</td>
+                </tr>
+                <tr>
+                    <td>Saddam Arefin</td>
+                    <td>sarefin234</td>
+                    <td>arefinpolok@gmail.com</td>
+                    <td>Male</td>
+                    <td>Kustia</td>
+                </tr>
+                <tr>
+                    <td>Sayma Wazed</td>
+                    <td>oishi31</td>
+                    <td>sw02oishi@gmail.com</td>
+                    <td>Female</td>
+                    <td>Kishoregonj</td>
+                </tr>
+            </tbody>
+      </table>
+        `;
+        const infoPart = document.getElementById("infoPart");
+        infoPart.innerHTML = " ";
+        infoPart.appendChild(customerTable);
+    });
+    const addSingleCustomer = document.getElementById("addCustomer");
+    
     
 }
 
